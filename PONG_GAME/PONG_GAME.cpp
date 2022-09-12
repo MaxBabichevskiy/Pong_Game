f0f4641fbd75bf12bd236f4fca82933436303397
Max Babichevskyi <100165364+MaxBabichevskiy@users.noreply.github.com>
2022-09-12 21:05:07 +0300

    Add files via upload
    Release Manager

--- PONG_GAME/PONG_GAME.cpp
+++ PONG_GAME/PONG_GAME.cpp
@@ -1,4 +1,6 @@
 ﻿#include<iostream>
+#include<time.h>
+#include<conio.h>
 using namespace std;
 enum eDir { STOP = 0, LEFT = 1, UPLEFT = 2, DOWNLEFT = 3, RIGHT = 4, UPRIGHT = 5, DOWNRIGHT = 6};
 //Класс мяча
@@ -20,15 +22,9 @@ public:
 		direction = STOP;
 	}
 	//Изменение направления 
-	void changeDirection(eDir d) {
-		direction = d;
-	}
+	void changeDirection(eDir d) {direction = d;}
 	//Рандомное направление
-	void randomDirection() {
-
-		direction = (eDir)((rand() % 6) + 1);
-	
-	}
+	void randomDirection() { direction = (eDir)((rand() % 6) + 1);}
 	//Функция для перемещения
 	void Move() {
 		switch (direction)
@@ -56,7 +52,6 @@ public:
 		default:
 			break;
 		}
-
 	}
 	//Получение текущих координат
 	inline int getX() { return x; }
@@ -64,8 +59,7 @@ public:
 	//Получение текущего направления
 	inline eDir getDirection() { return direction; }
 	//Вывод обьекта через cout
-	friend ostream& operator <<(ostream& o, cBall c)
-	{
+	friend ostream& operator <<(ostream& o, cBall c){
 		o << "Ball [" << c.x << "," << c.y << "][" << c.direction << "]";
 		return o;
 	}
@@ -77,7 +71,6 @@ class cPaddle{
 public:
 	cPaddle() { x = y = 0; } //Текущее положение равно нулю
 	cPaddle(int posX, int posY) {
-		
 		originalX = posX;
 		originalY = posY;
 		x = posX;
@@ -92,23 +85,44 @@ public:
 	inline void moveUp() { y--; }
 	inline void moveDown() { y++; }
 	//Вывод обьекта через cout
-	friend ostream& operator <<(ostream& o, cPaddle c)
-	{
+	friend ostream& operator <<(ostream& o, cPaddle c){
 		o << "Paddle [" << c.x << "," << c.y << "]";
 		return o;
 	}
 };
+//Менеджер
+class cGameManager {
+	int width, height;
+	int score1, score2;
+	char up1, down1, up2, down2;
+	bool quit;
+	cBall* ball;
+	cPaddle* player1;
+	cPaddle* player2;
+public:
+	cGameManager(int w,int h) {
+		srand(time(NULL));
+		quit = false;
+		up1 = 'w'; up2 = 'u';
+		down1 = 's'; down2 = 'j';
+		score1 = score2 = 0;
+		width = w; height = h;
+		//Создание игроков и мяча
+		ball = new cBall(w / 2, h / 2);
+		player1 = new cPaddle(1, h / 2 - 3);
+		player2 = new cPaddle(w - 2, h / 2 - 3);
+	}
+	~cGameManager() { delete ball, player1, player2; }
+	void ScoreUp(cPaddle* player) {
+		if (player == player1) score1++;
+		else if (player == player2) score2++;
+		ball->Reset();
+		player1->Reset;
+		player2->Reset;
+	}
+};
 //Main
 int main() {
-	cPaddle p1(0, 0);
-	cPaddle p2(10, 0);
-	cout << p1 << endl;
-	cout << p2 << endl;
-	p1.moveUp();
-	p2.moveDown();
-	cout << p1 << endl;
-	cout << p2 << endl;
-
 
 	return 0;
 }
