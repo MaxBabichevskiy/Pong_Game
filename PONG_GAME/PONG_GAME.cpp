0509542bd8941bf0ad1377f4efce69f8825a758a
Max Babichevskyi <100165364+MaxBabichevskiy@users.noreply.github.com>
2022-09-12 20:34:49 +0300

    Add files via upload
    Realize Paddle

--- PONG_GAME/PONG_GAME.cpp
+++ PONG_GAME/PONG_GAME.cpp
@@ -69,19 +69,45 @@ public:
 		o << "Ball [" << c.x << "," << c.y << "][" << c.direction << "]";
 		return o;
 	}
-
 };
-
+//Класс плитки
+class cPaddle{
+	int x, y;
+	int originalX, originalY;
+public:
+	cPaddle() { x = y = 0; } //Текущее положение равно нулю
+	cPaddle(int posX, int posY) {
+		
+		originalX = posX;
+		originalY = posY;
+		x = posX;
+		y = posY;
+	}
+	//Сброс положения
+	inline void Reset() { x = originalX; y = originalY; }
+	//Получить координаты
+	inline int getX() { return x; }
+	inline int getY() { return y; }
+	//Передвижение плитки
+	inline void moveUp() { y--; }
+	inline void moveDown() { y++; }
+	//Вывод обьекта через cout
+	friend ostream& operator <<(ostream& o, cPaddle c)
+	{
+		o << "Paddle [" << c.x << "," << c.y << "]";
+		return o;
+	}
+};
+//Main
 int main() {
-	cBall c(0, 0);
-	cout << c << endl;
-	c.randomDirection();
-	cout << c << endl;
-	c.Move();
-	cout << c << endl;
-	c.randomDirection();
-	c.Move();
-	cout << c << endl;
+	cPaddle p1(0, 0);
+	cPaddle p2(10, 0);
+	cout << p1 << endl;
+	cout << p2 << endl;
+	p1.moveUp();
+	p2.moveDown();
+	cout << p1 << endl;
+	cout << p2 << endl;
 
 
 	return 0;
