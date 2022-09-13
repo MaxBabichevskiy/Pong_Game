32efa4971ae453d04c68641d242204ac2e98cbe0
Max Babichevskyi <100165364+MaxBabichevskiy@users.noreply.github.com>
2022-09-13 11:17:26 +0300

    Add files via upload
    Final Release

--- PONG_GAME/PONG_GAME.cpp
+++ PONG_GAME/PONG_GAME.cpp
@@ -1,6 +1,8 @@
 ﻿#include<iostream>
 #include<time.h>
 #include<conio.h>
+#include<Windows.h>
+
 using namespace std;
 enum eDir { STOP = 0, LEFT = 1, UPLEFT = 2, DOWNLEFT = 3, RIGHT = 4, UPRIGHT = 5, DOWNRIGHT = 6};
 //Класс мяча
@@ -103,8 +105,8 @@ public:
 	cGameManager(int w,int h) {
 		srand(time(NULL));
 		quit = false;
-		up1 = 'w'; up2 = 'u';
-		down1 = 's'; down2 = 'j';
+		up1 = 'w'; up2 = 'i';
+		down1 = 's'; down2 = 'k';
 		score1 = score2 = 0;
 		width = w; height = h;
 		//Создание игроков и мяча
@@ -120,17 +122,13 @@ public:
 		player1->Reset();
 		player2->Reset();
 	}
-	void Draw()
-	{
+	void Draw(){
 		system("cls");
-		for (int i = 0; i < width + 2; i++)
-			cout << "\xB2";
+		for (int i = 0; i < width + 2; i++)cout << "\xB2";
 		cout << endl;
 
-		for (int i = 0; i < height; i++)
-		{
-			for (int j = 0; j < width; j++)
-			{
+		for (int i = 0; i < height; i++){
+			for (int j = 0; j < width; j++){
 				int ballx = ball->getX();
 				int bally = ball->getY();
 				int player1x = player1->getX();
@@ -138,8 +136,7 @@ public:
 				int player1y = player1->getY();
 				int player2y = player2->getY();
 
-				if (j == 0)
-					cout << "\xB2";
+				if (j == 0) cout << "\xB2";
 
 				if (ballx == j && bally == i)
 					cout << "O"; //Мяч
@@ -147,20 +144,20 @@ public:
 					cout << "\xDB"; //игрок 1
 				else if (player2x == j && player2y == i)
 					cout << "\xDB"; //игрок 2
-
+				//игрок 1 рисование плитки
 				else if (player1x == j && player1y + 1 == i)
-					cout << "\xDB"; //игрок 1
+					cout << "\xDB"; 
 				else if (player1x == j && player1y + 2 == i)
-					cout << "\xDB"; //игрок 1
+					cout << "\xDB"; 
 				else if (player1x == j && player1y + 3 == i)
-					cout << "\xDB"; //игрок 1
-
+					cout << "\xDB"; 
+				//игрок 2 рисование плитки
 				else if (player2x == j && player2y + 1 == i)
-					cout << "\xDB"; //игрок 2
+					cout << "\xDB"; 
 				else if (player2x == j && player2y + 2 == i)
-					cout << "\xDB"; //игрок 2
+					cout << "\xDB"; 
 				else if (player2x == j && player2y + 3 == i)
-					cout << "\xDB"; //игрок 2
+					cout << "\xDB"; 
 				else
 					cout << " ";
 
@@ -169,18 +166,94 @@ public:
 			}
 			cout << endl;
 		}
-
 		for (int i = 0; i < width + 2; i++)
 			cout << "\xB2";
 		cout << endl;
+		cout << "Score 1: " << score1 << "\t\t" << "Score 2: " << score2 << endl;
+	}
 
-		cout << "Score 1: " << score1 << endl << "Score 2: " << score2 << endl;
+	void Input(){
+		ball->Move();
+
+		int ballx = ball->getX();
+		int bally = ball->getY();
+		int player1x = player1->getX();
+		int player2x = player2->getX();
+		int player1y = player1->getY();
+		int player2y = player2->getY();
+
+		if (_kbhit()){
+			char current = _getch();
+			if (current == up1)
+				if (player1y > 0)
+					player1->moveUp();
+			if (current == up2)
+				if (player2y > 0)
+					player2->moveUp();
+			if (current == down1)
+				if (player1y + 4 < height)
+					player1->moveDown();
+			if (current == down2)
+				if (player2y + 4 < height)
+					player2->moveDown();
+
+			if (ball->getDirection() == STOP)
+				ball->randomDirection();
+
+			if (current == 'q')
+				quit = true;
+		}
+	}
+	void Logic()
+	{
+		int ballx = ball->getX();
+		int bally = ball->getY();
+		int player1x = player1->getX();
+		int player2x = player2->getX();
+		int player1y = player1->getY();
+		int player2y = player2->getY();
+
+		//Левая плитка
+		for (int i = 0; i < 4; i++)
+			if (ballx == player1x + 1)
+				if (bally == player1y + i)
+					ball->changeDirection((eDir)((rand() % 3) + 4));
+
+		//Правая плитка
+		for (int i = 0; i < 4; i++)
+			if (ballx == player2x - 1)
+				if (bally == player2y + i)
+					ball->changeDirection((eDir)((rand() % 3) + 1));
+
+		//Нижняя стена
+		if (bally == height - 1)
+			ball->changeDirection(ball->getDirection() == DOWNRIGHT ? UPRIGHT : UPLEFT);
+		
+		//Верхняя стена
+		if (bally == 0)
+			ball->changeDirection(ball->getDirection() == UPRIGHT ? DOWNRIGHT : DOWNLEFT);
+		
+		//Правая стена
+		if (ballx == width - 1)
+			ScoreUp(player1);
+		
+		//Левая стена
+		if (ballx == 0)
+			ScoreUp(player2);
+	}
+	void Run(){
+		while (!quit){
+			Draw();
+			Input();
+			Logic();
+			Sleep(60);
+		}
 	}
 };
 //Main
 int main() {
-
+	
 	cGameManager c(40, 20);
-	c.Draw();
+	c.Run();
 	return 0;
 }
